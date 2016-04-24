6bdb99a19c97cb7ac798c9e43acc3809727fb14b
Ben Babtsov <ben.babtsov@ufl.edu>
2016-04-23 23:47:13 -0400

    initial commit

---
+++ WSN_Net/Credentials.h
@@ -0,0 +1,9 @@
+#ifndef __CREDENTIALS_H__
+#define __CREDENTIALS_H__
+
+// Wifi parameters
+char passphrase[] = "";
+char ssid[] = "ufvisitor";
+char ntp_server[] = "nist1-la.ustiming.org"; // or your favorite ntp server.
+
+#endif
